36e1871ce15614ea1c6dd8d731c641d90cdc72ce
gedewon <gedewon.hayle.9@gmail.com>
2023-01-14 06:06:41 +0300

    Create and simulate api response on client side

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import axios from 'axios';
 
 function App() {
   const [searchTerm,setSearchTerm] = useState("");
-  let searchResults = [];
+  const [searchResults,setSearchResults] = useState([]);
 
+  useEffect(() => {
+    let isDisposed = false;
 
+    (async()=>{
+      const response =  await axios.get(`/api/v1/search?q=${searchTerm}`);
 
+      if(!isDisposed) setSearchResults(response.data);
+    })();
+
+    return () => {
+      isDisposed = true;
+    }
+  }, [searchTerm])
+  
+ 
+  const handleClick = useCallback(
+    (e) => {
+      setSearchTerm(e.target.innerHTML)
+    },
+    [],
+  );
 
   return (
     <main>
       <h1>Welcome</h1>
-      <input type="text" placeholder='search input'/>
+      <input type="text" placeholder='search input' onChange={(e)=>setSearchTerm(e.target.value)} value={searchTerm}/>
+      <ul>
       {searchResults.length > 0 && (
-        searchResults.map((element, index)=><p key={index}>{element}</p>)
+        searchResults.map((element, index)=><li key={index} onClick={handleClick}>{element}</li>)
         )
       } 
+      </ul>
    </main>
     );
 }
