404fe96f2da6f2ba1cf6ef82c011eefbad4bbbbb
gedewon <gedewon.hayle.9@gmail.com>
2023-01-14 04:55:25 +0300

    Create simple search box

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+
+
 function App() {
-    return (<h1>Hello World!</h1>);
-  }
+  const [searchTerm,setSearchTerm] = useState("");
+  let searchResults = [];
+
+
+
+
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <input type="text" placeholder='search input'/>
+      {searchResults.length > 0 && (
+        searchResults.map((element, index)=><p key={index}>{element}</p>)
+        )
+      } 
+   </main>
+    );
+}
+
+
 const root = createRoot(document.getElementById('root'));
 root.render(<App/>);
 
