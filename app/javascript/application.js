f82355666eafd42e3da0fdfd85b39468eca2e910
gedewon <gedewon.hayle.9@gmail.com>
2023-01-15 18:20:24 +0300

    Handle search per specific user

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,13 +4,15 @@ import axios from 'axios';
 
 function App() {
   const [searchTerm,setSearchTerm] = useState("");
-  const [searchResults,setSearchResults] = useState([]);
+  const [searchResults,setSearchResults] = useState({});
+  const [userId,setUserId] = useState(null);
 
   useEffect(() => {
     let isDisposed = false;
 
     (async()=>{
-      const response =  await axios.get(`/api/v1/search?q=${searchTerm}`);
+      const userId = localStorage.getItem('userId')
+      const response =  await axios.get(`/api/v1/search?q=${searchTerm}&user=${userId}`);
 
       if(!isDisposed) setSearchResults(response.data);
     })();
@@ -20,6 +22,29 @@ function App() {
     }
   }, [searchTerm])
   
+
+  useEffect(() => {
+    
+    let isDisposed = false;
+
+    if(!localStorage.getItem('userId')){
+      //create a simple user here
+      (async()=>{
+        const response = await axios.get(`/api/v1/user`);
+        const userId = response.data
+        if(!isDisposed) {
+          setUserId(userId);
+          localStorage.setItem('userId',JSON.stringify(userId))
+        }
+      })();
+    }
+    
+    
+    return () => {
+      isDisposed=true;
+    }
+  }, [])
+  
  
   const handleClick = useCallback(
     (e) => {
@@ -27,18 +52,24 @@ function App() {
     },
     [],
   );
+   
+  let sortedUserSearchResult = useMemo(()=>searchResults?.user_history?.sort((a,b)=>b[1]-a[1]),[searchResults?.user_history]);
+  let sortedGlobalSearchResult =  useMemo(()=>searchResults?.global_history?.sort((a,b)=>b[1]-a[1]),[searchResults?.global_history]);
 
-  let sortedSearchResult = useMemo(() => searchResults.sort((a,b)=>b[1]-a[1]), [searchResults])
-
+ 
   return (
     <main>
       <h1>Welcome</h1>
       <input type="text" placeholder='search input' onChange={(e)=>setSearchTerm(e.target.value)} value={searchTerm}/>
       <ul>
-      {sortedSearchResult.length > 0 && (
-        searchResults.map((element, index)=><li key={index} onClick={handleClick}>{element}</li>)
+      {sortedUserSearchResult?.length > 0 && (
+        sortedUserSearchResult.map((element, index)=><li key={index} className='history' onClick={handleClick}>{element}</li>)
         )
-      } 
+      }
+      {sortedGlobalSearchResult?.length > 0 && (
+        sortedGlobalSearchResult?.map((element, index)=><li key={index} onClick={handleClick}>{element}</li>)
+        )
+      }  
       </ul>
    </main>
     );
