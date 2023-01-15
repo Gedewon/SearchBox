2f7b59270829ffecc81fb2697106ce7e48b22185
gedewon <gedewon.hayle.9@gmail.com>
2023-01-15 10:14:18 +0300

    Create Search box spec

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,49 @@
 class Api::V1::SearchController < ApplicationController
+  require 'trie'
+
+
+  # build trie if it's not in memory already
+  # when we finish the search query we can update the frequency of the query or register a new query
+  # get a list of auto-completion while typing in real time
   def index
-    @a =  ['sofi','sssfadf','sdfasfds']
-   render json: @a.select {|ele| ele.include?(params[:q])}
+   build unless @trie
+
+   insert_or_update(params[:q]) if params[:q].include?('?')
+
+   @list = @trie.find_words_starting_with(params[:q].capitalize)
+   
+   return render json:[] if params[:q].eql?('')
+   render json: @list
+  end
+
+
+  # get analytic data from DB and build the trie for fast 
+  # search on the query and get an auto-completion list
+  private 
+  def build 
+    @trie = Trie.new();
+    @analytics = Analytic.all
+
+     @analytics.each do |analytic| 
+      @trie.insert(analytic.query.capitalize, analytic.frequency);
+    end
+  end
+
+  # insert a new query or update the existing query frequency
+  # and rebuild the trie in memory
+  def insert_or_update(query)
+      return unless query.include?('?')
+
+      clean_string = query[0...-1]
+     
+      if @trie.search(clean_string.capitalize)
+        analytic = Analytic.find_by(query:clean_string.capitalize)
+        analytic.frequency= analytic.frequency+1
+        analytic.save
+      else
+        Analytic.create(query:clean_string.capitalize, frequency:1)
+      end
+
+      build
   end
 end
---
+++ spec/requests/api/v1/search_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Searches', type: :request do
+  describe 'GET /index' do
+    before(:example) do
+      @analytic = Analytic.create(query: 'test query', frequency: 1)
+    end
+    it 'returns a 200' do
+      get '/api/v1/search', params: { q: 'a' }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'empty query should not search trie' do
+      get '/api/v1/search', params: { q: '' }
+      expect(response.body).to eql('[]')
+    end
+
+    it 'valid query string should search the trie and retun capitalize version and frequency count' do
+      get '/api/v1/search', params: { q: 'test query' }
+      expect(response.body).to eql('[["Test query",1]]')
+    end
+
+    it 'new query ending in ? should be persisted to db and rebuild the trie' do
+      get '/api/v1/search', params: { q: 'new test query?' }
+      expect(response.body).to eql('[["New test query",1]]')
+    end
+  end
+end
