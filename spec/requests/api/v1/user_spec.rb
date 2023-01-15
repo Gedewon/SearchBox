7938ce55d5eec74cadc9c24060c096d28cc32488
gedewon <gedewon.hayle.9@gmail.com>
2023-01-15 19:17:31 +0300

    Create User controller spec

--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/api/v1/user/index'
+      get '/api/v1/user'
       expect(response).to have_http_status(:success)
+    end
+    it 'return new user per call' do
+      get '/api/v1/user'
+      expect(response.body).to eql(User.last.id.to_s)
     end
   end
 end
