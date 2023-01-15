eaeba9e529c5ebc9c0cee81b5cf18a88974e5ce1
gedewon <gedewon.hayle.9@gmail.com>
2023-01-15 19:18:56 +0300

    Fix linter locally

--- spec/requests/api/v1/search_spec.rb
+++ spec/requests/api/v1/search_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe 'Api::V1::Searches', type: :request do
   describe 'GET /index' do
     before(:example) do
       @analytic = Analytic.create(query: 'test query', frequency: 1)
-      @user = User.create()
-      @history = SearchHistorie.create(user_id:@user.id, query:'test query', frequency:1)
+      @user = User.create
+      @history = SearchHistorie.create(user_id: @user.id, query: 'test query', frequency: 1)
     end
     it 'returns a 200' do
-      get '/api/v1/search', params: { q: 'a' ,user:@user}
+      get '/api/v1/search', params: { q: 'a', user: @user }
       expect(response).to have_http_status(:ok)
     end
 
@@ -19,12 +19,12 @@ RSpec.describe 'Api::V1::Searches', type: :request do
 
     it 'valid query string should search the trie and retun capitalize version and frequency count' do
       get '/api/v1/search', params: { q: 'test query', user: @user.id }
-      expect(response.body).to eql("{\"global_history\":[[\"Test query\",1]],\"user_history\":[[\"Test query\",1]]}")
+      expect(response.body).to eql('{"global_history":[["Test query",1]],"user_history":[["Test query",1]]}')
     end
 
     it 'new query ending in ? should be persisted to db and rebuild the trie' do
-      get '/api/v1/search', params: { q: 'new test query?', user:@user.id }
-      expect(response.body).to eql("{\"global_history\":[[\"New test query\",1]],\"user_history\":[[\"New test query\",1]]}")
+      get '/api/v1/search', params: { q: 'new test query?', user: @user.id }
+      expect(response.body).to eql('{"global_history":[["New test query",1]],"user_history":[["New test query",1]]}')
     end
   end
 end
